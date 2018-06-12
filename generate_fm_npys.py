d5e40121078971591f1101b59a43aba73fab4380
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2018-06-12 22:42:29 +0200

    Add reading configuration from YAML

--- generate_fm_npys.py
+++ generate_fm_npys.py
@@ -1,11 +1,14 @@
-from torch import nn, Tensor
-from torchvision import models
-import numpy as np
-from cyvlfeat.gmm import gmm
-from sklearn.svm import SVC
-from skimage import io
 from glob import glob
+from pathlib import Path
+
+import numpy as np
 import pandas as pd
+import yaml
+from cyvlfeat.gmm import gmm
+from skimage import io
+from sklearn.svm import SVC
+from torch import Tensor, nn
+from torchvision import models
 
 
 def model_init():
@@ -17,7 +20,8 @@ def model_init():
 
 def image_read(path):
     image = io.imread(path)
-    image = np.reshape(image, (1, image.shape[2], image.shape[0], image.shape[1]))
+    image = np.reshape(
+        image, (1, image.shape[2], image.shape[0], image.shape[1]))
     image_tensor = Tensor(np.asarray(image / 256).astype(np.uint8))
     return image_tensor
 
@@ -37,7 +41,13 @@ def prepare_one_image_to_classify(path):
     data = extract_features(image_tensor, model)
     return data
 
-paths = glob('/home/dawid_rymarczyk/Pobrane/fungus/*/*')
+
+config_path = Path('config.yml')
+config = None
+with config_path.open('r') as f:
+    config = yaml.load(f)
+
+paths = glob(config['data_path'] + '/*/*')
 paths_for_train = []
 paths_for_test = []
 for path in paths:
@@ -49,4 +59,6 @@ for path in paths:
 if __name__ == '__main__':
     for path in paths_for_train:
         features_for_svm = prepare_one_image_to_classify(path)
+        print(features_for_svm)
+        break
         np.save(path.split('/')[-1][:-4], features_for_svm)
