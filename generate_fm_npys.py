b4be0ecb5dedf948ee4db674045eeb8c44f2f886
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2018-06-22 00:35:53 +0200

    Implement feature extraction using AlexNet

--- generate_fm_npys.py
+++ generate_fm_npys.py
@@ -1,38 +1,26 @@
+import sys
 from glob import glob
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import torch
 import yaml
 from cyvlfeat.gmm import gmm
 from skimage import io
 from sklearn.svm import SVC
-from torch import Tensor, nn
+from torch import nn
 from torchvision import models
 
 
-def model_init():
-    model = models.alexnet(pretrained=True)
-    # new_classifier = nn.Sequential(*list(model.features()))
-    # model.classifier = new_classifier
-    return model
-
-
-def image_read(path):
-    image = io.imread(path)
-    image = np.reshape(
-        image, (1, image.shape[2], image.shape[0], image.shape[1]))
-    image_tensor = Tensor(np.asarray(image / 256).astype(np.uint8))
-    return image_tensor
-
-
-def extract_features(image_tensor, model):
-    data = model.features(image_tensor)
-    del model, image_tensor
-    data = data.detach().numpy()[0]
-    data = data.reshape(data.shape[0], data.shape[2] * data.shape[1])
-    data = gmm(data.transpose())
-    return data
+def read_image(path):
+    image = io.imread(path).astype(np.float32)
+    # Move channels to the first dimension
+    image = np.moveaxis(image, -1, 0)
+    # Normalize to [0, 1]
+    image /= 256
+    tensor = torch.from_numpy(image)
+    return tensor
 
 
 def prepare_one_image_to_classify(path):
@@ -42,22 +30,57 @@ def prepare_one_image_to_classify(path):
     return data
 
 
-config_path = Path('config.yml')
-config = None
-with config_path.open('r') as f:
-    config = yaml.load(f)
+def read_config():
+    config_path = Path('config.yml')
+    config = None
+    with config_path.open('r') as f:
+        config = yaml.load(f)
+    return config
 
-paths = glob(config['data_path'] + '/*/*')
-paths_for_train = []
-paths_for_test = []
-for path in paths:
-    if int(path.split('/')[-1][2:-4]) > 9:
-        paths_for_test.append(path)
-    else:
-        paths_for_train.append(path)
-print('Found {} files for training.'.format(len(paths_for_train)))
+
+def split_data_paths(config):
+    paths = glob(config['data_path'] + '/*/*')
+    train_paths = []
+    test_paths = []
+    for path in paths:
+        if int(path.split('/')[-1][2:-4]) > 9:
+            test_paths.append(path)
+        else:
+            train_paths.append(path)
+    print('Found {} files for training.'.format(
+        len(train_paths)), file=sys.stderr)
+    print('Found {} files for testing.'.format(
+        len(test_paths)), file=sys.stderr)
+    return (train_paths, test_paths)
+
+
+def get_feature_extractor():
+    model = models.alexnet(pretrained=True)
+    extractor = model.features
+    return extractor
+
+
+def extract_features(extractor, images):
+    # We want to return a flat list of features
+    return extractor(images).reshape(images.size()[0], -1)
+
 
 if __name__ == '__main__':
-    for path in paths_for_train:
-        features_for_svm = prepare_one_image_to_classify(path)
-        np.save(path.split('/')[-1][:-4], features_for_svm)
+    config = read_config()
+    print('Splitting data paths...')
+    train_paths, test_paths = split_data_paths(config)
+    print('Getting feature extractor...')
+    feature_extractor = get_feature_extractor()
+    feature_extractor.eval()
+    with torch.no_grad():
+        print('Extracting features...')
+        train_features = []
+        for path in train_paths:
+            image = torch.unsqueeze(read_image(path), dim=0)
+            image_features = extract_features(feature_extractor, image)
+            train_features.append(image_features)
+        train_features = torch.cat(train_features, dim=0)
+        print(train_features.size())
+        print('Fitting gmm...')
+        priors, means, covars, ll, posteriors = gmm(
+            train_features, n_clusters=1, init_mode='rand')
