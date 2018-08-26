9d760c6594736ce32458e0bf5c7925bd6282516b
Adam Piekarczyk <adam.jan.piekarczyk@gmail.com>
2018-08-26 13:53:59 +0200

    Add tool config files

---
+++ watch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+inotifywait -r -e close_write,moved_to,create -m . |
+while read -r directory events filename; do
+    rsync -a . arccha@kogni3.ii.uj.edu.pl:~/fungus --exclude config.yml --exclude results
+done
