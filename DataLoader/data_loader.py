41e8091c5ff80020467e0291d37578cef16cd0a7
Adam Piekarczyk <adam.jan.piekarczyk@gmail.com>
2018-11-05 17:07:20 +0100

    Refactor DataLoader

--- DataLoader/data_loader.py
+++ DataLoader/data_loader.py
@@ -64,6 +64,8 @@ class FungusDataset(Dataset):
         self.train = train
         self.pngs_dir = pngs_dir
         self.masks_dir = masks_dir
+        if not self.pngs_dir or not self.masks_dir:
+            raise AttributeError('Paths to pngs and masks must be provided.')
         if self.train:
             self.paths = train_paths
         else:
@@ -74,19 +76,23 @@ class FungusDataset(Dataset):
         return len(self.paths) * (self.fg_per_img + self.bg_per_img)
 
     def _read_mask(self, idx):
-        mask_path = self.paths[int(
-            idx / (self.bg_per_img + self.fg_per_img))]
-        if self.masks_dir is not None:
-            mask_path = os.path.join(self.masks_dir, mask_path)
-        mask = io.imread(mask_path)
-        return mask
+        mask_path = self.paths[idx]
+        mask_path = os.path.join(self.masks_dir, mask_path)
+        return io.imread(mask_path)
+
+    def _read_image_and_class(self, idx):
+        path = self.paths[idx]
+        image_class = path.split('/')[-1][:2]
+        path = os.path.join(self.pngs_dir, path)
+        return io.imread(path), image_class
 
     def _is_foreground_patch(self, idx):
         return (idx % (self.bg_per_img + self.fg_per_img)) > self.bg_per_img
 
     def __getitem__(self, idx):
-        image, img_class = self._read_image_and_class(idx)
-        mask = self._read_mask(idx)
+        index = idx // (self.bg_per_img + self.fg_per_img)
+        image, image_class = self._read_image_and_class(index)
+        mask = self._read_mask(index)
 
         # set appropriate offsets in order to choose only full sized patches
         mask[:self.random_crop_size, :] = ImageSegment.NONE
@@ -94,24 +100,29 @@ class FungusDataset(Dataset):
         mask[:, :self.random_crop_size] = ImageSegment.NONE
         mask[:, -self.random_crop_size:] = ImageSegment.NONE
 
+        # prepare a set of coordinates to randomly choose patch center
         if self._is_foreground_patch(idx):
             if ImageSegment.FOREGROUND in mask:
                 where = np.argwhere(mask == ImageSegment.FOREGROUND)
             else:
                 warnings.warn(
-                    'Should generate foreground patch from {} class, but no foreground found in mask. Toggling to background.'.format(img_class))
+                    ('Should generate foreground patch from {} class, ' +
+                     'but no foreground found in mask. ' +
+                     'Toggling to background.').format(image_class))
                 where = np.argwhere(mask == ImageSegment.BACKGROUND)
-                img_class = 'BG'
+                image_class = 'BG'
         else:
             if ImageSegment.BACKGROUND in mask:
                 where = np.argwhere(mask == ImageSegment.BACKGROUND)
-                img_class = 'BG'
-
+                image_class = 'BG'
             else:
                 warnings.warn(
-                    'Should generate background patch from {} class, but no background found in mask. Toggling to foreground.'.format(img_class))
+                    ('Should generate background patch from {} class, ' +
+                     'but no background found in mask. ' +
+                     'Toggling to foreground.').format(image_class))
                 where = np.argwhere(mask == ImageSegment.FOREGROUND)
 
+        # get a random patch
         center = np.random.uniform(high=where.shape[0])
         y, x = where[int(center)]
         image = image[y - self.random_crop_size:y + self.random_crop_size,
@@ -119,43 +130,7 @@ class FungusDataset(Dataset):
                       :]
         if self.transform:
             image = self.transform(image)
-
         return {
             'image': image,
-            'class': self.FUNGUS_TO_NUMBER[img_class],
+            'class': self.FUNGUS_TO_NUMBER[image_class],
         }
-
-    def _read_image_and_class(self, idx):
-        img_class = self.paths[int(
-            idx / (self.bg_per_img + self.fg_per_img))].split('/')[-1][:2]
-        path = self.paths[int(
-            idx / (self.bg_per_img + self.fg_per_img))]
-        if self.pngs_dir is not None:
-            path = os.path.join(self.pngs_dir, path)
-        image = io.imread(path)
-        return image, img_class
-
-
-if __name__ == '__main__':
-    from matplotlib import pyplot as plt
-    # data = FungusDataset(paths, scale=8)
-    # data = FungusDataset(random_crop_size=125, number_of_bg_slices_per_image=2, dir_with_pngs_and_masks=None)
-    # data = FungusDataset(random_crop_size=125, number_of_bg_slices_per_image=2,
-    #                      dir_with_pngs_and_masks='/home/dawid_rymarczyk/PycharmProjects/fungus', train=True)
-    # data = FungusDataset(paths)
-    # data = FungusDataset(paths, crop=8)
-    dl = DataLoader(data, batch_size=32, shuffle=True, num_workers=2)
-
-    for i_batch, sample_batched in enumerate(dl):
-        print(i_batch, sample_batched['image'].size(),
-              sample_batched['class'])
-
-        # observe 2nd batch and stop.
-        if i_batch == 1:
-            plt.figure()
-            plt.imshow(sample_batched['image'][8].numpy().transpose((1, 2, 0)))
-            plt.title(str(sample_batched['class'][8]))
-            plt.axis('off')
-            plt.ioff()
-            plt.show()
-            break
