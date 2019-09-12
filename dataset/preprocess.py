cfaf211318bfb194c3fa81c603f3ad7a71ce667d
Adam Piekarczyk <adam.jan.piekarczyk@gmail.com>
2019-09-12 12:19:12 +0200

    Implement tiff to npy conversion

---
+++ dataset/preprocess.py
@@ -0,0 +1,35 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+from skimage import exposure
+from skimage import io
+from tqdm import tqdm
+
+
+def process(dataset_dir, out_dir):
+    out_dir.mkdir(exist_ok=True, parents=True)
+    for image_path in tqdm(dataset_dir.glob('**/*.tif')):
+        # use freeimage plugin to obtain 16 bits per sample
+        img = io.imread(image_path, plugin='freeimage').astype(np.float)
+        img / (2 ** 16 - 1)
+        p5, p95 = np.percentile(img, (5, 95))
+        img = exposure.rescale_intensity(
+            img, in_range=(p5, p95), out_range=(0, 1))
+        kind, = image_path.parent.parts[-1:]
+        name = image_path.stem + '.npy'
+        save_path = out_dir / kind
+        save_path.mkdir(exist_ok=True, parents=True)
+        np.save(out_dir / kind / name, img)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'dataset_dir', type=Path, help='absolute path to directory with dataset')
+    parser.add_argument(
+        '--out_dir', type=Path, help='absolute path to output directory', default=None)
+    args = parser.parse_args()
+    if not args.out_dir:
+        args.out_dir = (args.dataset_dir / '../preprocessed').resolve()
+    process(args.dataset_dir, args.out_dir)
